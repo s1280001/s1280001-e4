60e44cd08626819f22b6f54b8544fa8092c266af
s1280001 <s1280001@u-aizu.ac.jp>
2021-07-04 14:27:09 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,9 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 class DiceGame{
     public static void main(String[] args){
 	Random rand = new Random();
 	int[] data = new int[2];
 	int sum = 0;
+	 InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+ 
+        System.out.println("What is your name?");
+ 
+        String str = null;
+        try {
+	    System.out.print(">");
+            str = br.readLine();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+        System.out.println("Hello!" + str);
 
  
 	for(int i = 0;i<2;i++){
